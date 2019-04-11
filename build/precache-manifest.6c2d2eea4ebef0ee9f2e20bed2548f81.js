8b8408120dcf45fc7b348771d4bc550c54cee867
David <534102725@qq.com>
2019-04-11 19:17:10 +0800

    new

---
+++ build/precache-manifest.6c2d2eea4ebef0ee9f2e20bed2548f81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "77edd172d752bbeffb08",
+    "url": "./static/js/main.a358bc33.chunk.js"
+  },
+  {
+    "revision": "d1b4cae25a658f75b2b5",
+    "url": "./static/js/2.82d0536a.chunk.js"
+  },
+  {
+    "revision": "d1b4cae25a658f75b2b5",
+    "url": "./static/css/2.e4e5e0b6.chunk.css"
+  },
+  {
+    "revision": "7da860b8425731844dc14c631c23b9df",
+    "url": "./index.html"
+  }
+];
